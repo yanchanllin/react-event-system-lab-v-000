50a97b512513d7f893b0ca26c72e642e0cfae885
Yan C Lin <yonalin1@gmail.com>
2019-07-24 20:26:10 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 
 class EyesOnMe extends React.Component {
@@ -20,3 +21,24 @@ class EyesOnMe extends React.Component {
 };
 
 export default EyesOnMe;
+=======
+import React, { Component } from 'react';
+
+export default class EyesOnMe extends Component {
+
+   focus= () => {
+     console.log('Good!')
+   }
+    blur = () => {
+     console.log('Hey! Eyes on me!')
+  }
+  render() {
+    return (
+      <div className="eyes-on-me"
+      <button onFocus={this.focus} onBlur={this.blur} > hey there?</button>
+      </div>
+    )
+  }
+
+}
+>>>>>>> 64c42157374b2acbc352a673dc9fb78460203613
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 
 class Keypad extends React.Component {
@@ -10,9 +11,27 @@ class Keypad extends React.Component {
     return (
       <div className="keypad">
         <input type="password" onKeyUp={this.entering} />
+=======
+import React, { Component } from 'react';
+
+export default class Keypad extends Component {
+
+   Entering = () => {
+     console.log('Entering password...')
+   }
+
+  render() {
+    return (
+      <div className="keyboard"
+      <input type= "password" onKeyUp={this.entering}/>
+>>>>>>> 64c42157374b2acbc352a673dc9fb78460203613
       </div>
     )
   }
 
+<<<<<<< HEAD
 };
 export default Keypad;
+=======
+}
+>>>>>>> 64c42157374b2acbc352a673dc9fb78460203613
